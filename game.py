45e991c2e6d852f48ff3d75c0ba089a9f6c9a587
Drew Tolliver <henaege@gmail.com>
2017-04-28 16:04:46 -0400

    Going to push the same code Yingrong pushed

--- game.py
+++ game.py
@@ -2,52 +2,7 @@ import pygame
 from textbox import TextBox
 import sys
 from textbox_script import input_questions
-from scene import Scene
-<<<<<<< HEAD
-
-pygame.init()
-
-screen_size = (1200, 750)
-
-title_screen = pygame.image.load('./images/house.jpg')
-scene1_bg = pygame.image.load('./images/scene1_bg.jpg')
-scene2_bg = pygame.image.load('./images/scene2_bg.jpg')
-start_button = pygame.image.load('./images/start_button.png')
-start_button_hover = pygame.image.load('./images/start_button_hover.png')
-quit_button = pygame.image.load('./images/quit_button.png')
-quit_button_hover = pygame.image.load('./images/quit_button_hover.png')
-instructions_button = pygame.image.load('./images/instructions_button.png')
-instructions_button_hover = pygame.image.load('./images/instructions_button_hover.png')
-text_box = pygame.image.load('./images/text_box.png')
-woman = pygame.image.load('./images/beautiful_woman.png')
-elderly_man = pygame.image.load('./images/elderly_man.png')
-goth_teen = pygame.image.load('./images/goth_teen.png')
-
-
-
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Mystery House")
-intro = Scene(screen, title_screen, start_button)
-
-def run_game():
-    # pygame.init()
-
-    # screen_size = (1200, 750)
-git commit
-    # title_screen = pygame.image.load('./images/house.jpg')
-    # scene1_bg = pygame.image.load('./images/scene1_bg.jpg')
-    # scene2_bg = pygame.image.load('./images/scene2_bg.jpg')
-    # start_button = pygame.image.load('./images/start_button.png')
-    # start_button_hover = pygame.image.load('./images/start_button_hover.png')
-    # quit_button = pygame.image.load('./images/quit_button.png')
-    # quit_button_hover = pygame.image.load('./images/quit_button_hover.png')
-    # instructions_button = pygame.image.load('./images/instructions_button.png')
-    # instructions_button_hover = pygame.image.load('./images/instructions_button_hover.png')
-    # text_box = pygame.image.load('./images/text_box.png')
-    # woman = pygame.image.load('./images/beautiful_woman.png')
-    # elderly_man = pygame.image.load('./images/elderly_man.png')
-    # goth_teen = pygame.image.load('./images/goth_teen.png')
-=======
+from scene import Scene, DrivingScene
 # import classes
 
 def run_game():
@@ -57,9 +12,8 @@ def run_game():
     screen = pygame.display.set_mode(screen_size)
     screen_rect = screen.get_rect()
     pygame.display.set_caption("Mystery House")
-    background_img = pygame.image.load("images/house.jpg")
-    intro = Scene(screen, background_img)
->>>>>>> 17ab679cdcbe49c1288432eb614fafa0b20027c2
+    intro = Scene(screen)
+    driving = DrivingScene(screen)
 
     # settings = {
     # "text": input_questions["driving_scece1"],
--- scene.py
+++ scene.py
@@ -1,54 +1,37 @@
 import pygame
 
+
 button_list = {'start': ['./images/start_button_hover.png', './images/start_button.png'],
-        'quit': ['./images/quit_button_hover.png', './images/quit_button.png']
+        'quit': ['./images/quit_button_hover.png', './images/quit_button.png'],
+        'instruction': ['./images/Instructions_button_hover.png', './images/Instructions_button.png']
         }
 action = ['next', 'quit', 'instruction']
 
 
 class Scene(object):
-<<<<<<< HEAD
-    def __init__(self, screen, bg, button, status=False):
-=======
-    def __init__(self, screen, bg, status=False):
->>>>>>> 17ab679cdcbe49c1288432eb614fafa0b20027c2
+    def __init__(self, screen, status=False):
         self.screen = screen
-        self.bg = bg
+        self.bg = pygame.image.load("images/house.jpg")
         self.status = status
-
-class Intro(Scene):
-    def __init__(self):
-        super(Intro, self).__init__()
-        pass
-
 
     def enter(self):
         # while not self.status:
             self.screen.blit(self.bg, (0, 0))
-            self.create_button("start", 50, 50, action[0])
-            self.create_button("quit", 50, 50, action[1])
+            self.create_button("start", 400, 500, action[0])
+            self.create_button("quit", 400, 600, action[1])
             # self.screen.blit(text_box, (240, 600))
 
-<<<<<<< HEAD
-    def create_button(self, scene, x, y, action=None):
-        start_button = pygame.image.load('./images/start_button.png')
-        start_button_hover = pygame.image.load('./images/start_button_hover.png')
-        quit_button = pygame.image.load('./images/quit_button.png')
-        quit_button_hover = pygame.image.load('./images/quit_button_hover.png')
-        instructions_button = pygame.image.load('./images/instructions_button.png')
-        instructions_button_hover = pygame.image.load('./images/instructions_button_hover.png')
-=======
     def create_button(self, button, x, y, action=None):
         load_button_active = pygame.image.load(button_list[button][0])
         load_button_inactive = pygame.image.load(button_list[button][1])
->>>>>>> 17ab679cdcbe49c1288432eb614fafa0b20027c2
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         # print(click)
-        if load_button_active.get_rect(left=x, top=y).collidepoint(mouse):
-            self.screen.blit(load_button_inactive, (x, y))
+        if load_button_inactive.get_rect(left=x, top=y).collidepoint(mouse):
+            self.screen.blit(load_button_active, (x, y))
             if click[0] == 1 and action != None:
                 if action == "next":
+                    # subclass for this function
                     self.enter()
                     click[0] = False
                 if action == "quit":
@@ -56,3 +39,17 @@ class Intro(Scene):
                     quit()
         else:
             self.screen.blit(load_button_inactive, (x, y))
+
+
+class DrivingScene(Scene):
+
+    def __init__(self, screen):
+        super(DrivingScene, self).__init__(screen)
+        self.bg = pygame.image.load("images/driving1.jpg")
+        # self.script = script
+
+    def enter(self):
+        # while not self.status:
+            self.screen.blit(self.bg, (0, 0))
+            self.create_button("instruction", 400, 500, action[0])
+            self.create_button("quit", 400, 600, action[1])
